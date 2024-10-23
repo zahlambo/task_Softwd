1fe1daed133b9b2e0635bfc400eca5277d8b16ea
zahlambo <zahlambo@gmail.com>
2024-10-23 15:59:02 +0600

    little change

--- Allocar/main.py
+++ Allocar/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from core.db import get_database
 from models import *
-from routers import allocar, historical_reports,hrm
+from routers import allocar, hrm,reports
 
 app = FastAPI(title="Allocar",description="This is a api for allocating vehicles to employees.")
 db=get_database()
 app.include_router(allocar.router)
-app.include_router(historical_reports.router)
+app.include_router(reports.router)
 app.include_router(hrm.router)
 
 
